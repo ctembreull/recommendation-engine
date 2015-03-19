66a1915a31f4ee8a13951946a5d89dbe3826a807
Chris Tembreull <chris@tembreull.org>
2015-03-19 00:19:39 -0700

    keep a copy of the elasticsearch scripts we use

---
+++ scripts/cvg_score.groovy
@@ -0,0 +1,14 @@
+def convergence   = 0;
+def source_weight = 1 + src_weight;
+def target_weight = 1 + tgt_weight;
+def total_weight  = source_weight + target_weight;
+
+for (term in terms) {
+  convergence += _index["interests"][term].tf();
+}
+
+def u1_divergence = abs((convergence / terms.size()) -1);
+def u2_divergence = abs((convergence / doc["interests"].values.size()) -1);
+def divergence = 1 * (total_weight - ((u1_divergence / source_weight) + (u2_divergence / target_weight))) / total_weight;
+
+divergence;
