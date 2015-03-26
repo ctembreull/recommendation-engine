7395e85b66ad1a1426760c1a26056d841fccb18f
Chris Tembreull <chris@tembreull.org>
2015-03-26 16:28:09 -0700

    syntax error in seed

--- scripts/seed.rb
+++ scripts/seed.rb
@@ -1,6 +1,6 @@
 require 'elasticsearch'
 
-Dir[File.expand_path('../../lib/**/*.rb'), __FILE__)].each { |f| require f }
+Dir[File.expand_path('../../lib/**/*.rb', __FILE__)].each { |f| require f }
 Dir[File.expand_path('../../models/**/*.rb', __FILE__)].each { |f| require f }
 
 HOST  = 'localhost'
