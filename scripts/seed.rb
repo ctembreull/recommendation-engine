ecc8e3e2b316b09c6143e36f3eaf50ff5b9805f6
Chris Tembreull <chris@tembreull.org>
2015-03-26 16:31:34 -0700

    Mapping and Stub are modules, not methods.

--- scripts/seed.rb
+++ scripts/seed.rb
@@ -1,7 +1,7 @@
 require 'elasticsearch'
 
 Dir[File.expand_path('../../lib/**/*.rb', __FILE__)].each { |f| require f }
-Dir[File.expand_path('../../models/**/*.rb', __FILE__)].each { |f| require f }
+Dir[File.expand_path('../../models/*.rb', __FILE__)].each { |f| require f }
 
 HOST  = 'localhost'
 PORT  = 9200
@@ -10,14 +10,11 @@ TYPE  = 'person'
 
 es = Elasticsearch::Client.new log:false
 
-mapping = Comotion::Data::Person.Mapping.map
-# puts mapping
-
 es.indices.delete(index: INDEX) if es.indices.exists(index: INDEX)
-es.indices.create index: INDEX, type: 'user', body: Comotion::Data::Person.mapping
+es.indices.create index: INDEX, type: 'user', body: Comotion::Data::Person::Mapping.map
 
 1000.times do
-  user = Comotion::Data::Person.Stub.seed
+  user = Comotion::Data::Person::Stub.seed
   es.index index: INDEX, type: TYPE, id: user[:id], body: user
   # puts user
 end
